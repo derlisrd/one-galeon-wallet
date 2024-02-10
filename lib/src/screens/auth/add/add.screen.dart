e25e6cfe590fff9238e71661c403d898b6856cf5
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-02-10 00:12:22 -0300

    hp20

--- lib/src/screens/auth/add/add.screen.dart
+++ lib/src/screens/auth/add/add.screen.dart
@@ -2,27 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:onegaleon/src/widgets/buttons/secondary.button.dart';
 import 'package:onegaleon/src/widgets/index.dart';
 
-class AddScreen extends StatelessWidget {
+class AddScreen extends StatefulWidget {
   const AddScreen({super.key});
 
+  @override
+  State<AddScreen> createState() => _AddScreenState();
+}
 
+class _AddScreenState extends State<AddScreen> {
   
-
+  
+  
   @override
   Widget build(BuildContext context) {
-
+    
     void salir(){
-    Navigator.pop(context);
-  }
+      Navigator.pop(context);
+    }
 
     return  Scaffold(
-      body: Column(
-        children: [
-          const MontseText('Agregar un movimiento'),
-          const FieldPrimary(hintText: 'Valor',),
-          const PrimaryButton(text: 'Agregar'),
-          SecondaryButton(text: 'Cancelar',onTap: salir,)
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            const MontseText('Agregar un movimiento'),
+            const FieldPrimary(hintText: 'Descripción',),
+            const PrimaryButton(text: 'Agregar'),
+            SecondaryButton(text: 'Cancelar',onTap: salir)
+          ],
+        ),
       ),
     );
   }
